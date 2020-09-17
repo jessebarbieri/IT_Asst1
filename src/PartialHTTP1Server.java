c6cde943d35a60029c816a179c959e1bbb721ae0
Jason Wrobel <j.wrobel1999@gmail.com>
2020-09-17 14:56:41 -0400

    Test commit

--- src/PartialHTTP1Server.java
+++ src/PartialHTTP1Server.java
@@ -1,6 +1,6 @@
 public class PartialHTTP1Server {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to 352-Internet Technology");
+        System.out.println("Welcome to 352-Internet Technology :)");
     }
 }
