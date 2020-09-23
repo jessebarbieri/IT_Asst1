c1e810085e9f6d8afd02c8b82dc92b457eaa95f9
Jason Wrobel <j.wrobel1999@gmail.com>
2020-09-23 17:06:56 -0400

    Update Threads

--- src/PartialHTTP1Threads.java
+++ src/PartialHTTP1Threads.java
@@ -19,19 +19,34 @@ public class PartialHTTP1Threads extends Thread{
         //Output Stream Initialization
         try {
             //Gets outputstream and inputstream from socket (connection)
+
             PrintStream output = new PrintStream(this.connection.getOutputStream());
             BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 
+            //Waits 5 seconds for request from client
+            long startTime = System.currentTimeMillis(); //fetch starting time
+            while (connection.getInputStream().available() == 0 && System.currentTimeMillis() < startTime + 5000) {
+                continue;
+            }
+
+            //If no request is received, send 408 Request Timeout and exit thread
+            if(connection.getInputStream().available() == 0) {
+                output.print("HTTP/1.0 408 Request Timeout\r\n");
+                output.print("\r\n"); // End of headers
+                killThread();
+                return;
+            }
+
             //Tokenizer to get method / other data
             String inLine = input.readLine();
-            String testLine = input.readLine();
-//            if (!testLine.equals("")) {
-//                System.out.println("\n\n\n\n" + testLine + "\n\n\n");
-//            }
-            StringTokenizer test = new StringTokenizer(inLine);
-            while (test.hasMoreTokens()) {
-                System.err.println(test.nextToken());
+
+            //Checks for If-Modified-Since: 'date'
+            String ifModified = input.readLine();
+            String ifModifiedDate = "";
+            if (ifModified.indexOf("If-Modified-Since:") != -1) {
+                ifModifiedDate = ifModified.substring(19 , ifModified.length());
             }
+
             StringTokenizer tokenizer = new StringTokenizer(inLine);
 
             String method = tokenizer.nextToken();
@@ -80,7 +95,7 @@ public class PartialHTTP1Threads extends Thread{
                     return;
                 }
 
-                sendFile(output, fileURL);
+                sendFile(output, fileURL, ifModifiedDate);
                 killThread();
                 return;
 
@@ -119,13 +134,13 @@ public class PartialHTTP1Threads extends Thread{
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println("Thread terminating for --> " + this.connection.getRemoteSocketAddress());
+        //System.out.println("Thread terminating for --> " + this.connection.getRemoteSocketAddress());
         PartialHTTP1Server.thread_count--;
         return;
     }
 
     //Attempts to send file from server to client
-    private void sendFile(PrintStream output, String filename) {
+    private void sendFile(PrintStream output, String filename, String ifModifiedDate) {
         File file;
         try {
             //Creates file
@@ -145,24 +160,40 @@ public class PartialHTTP1Threads extends Thread{
             fileInput.read(data);
             fileInput.close();
 
-            //Sends 200 OK response header, sends file data, closes output stream, returns
-            output.print("HTTP/1.0 200 OK\r\n");
-            output.print("Content-Type: " + mimeType + "\r\n"); // TODO - add mime type support
-            output.print("Content-Length: " + file.length() + "\r\n");
-            System.out.println("Content Length....." + file.length());
-
             //Last-Modified response line
-            String temp = "";
-            try{ temp = convertDate(file.lastModified());}
+            String lastModified = "";
+            try{
+                lastModified = convertDate(file.lastModified());
+            }
             catch (Exception e){
                 e.printStackTrace();
             }
-            output.print("Last-Modified: " + temp + "\r\n");
+
+            boolean dateAfter = false;
+            if (!ifModifiedDate.equals("") && !lastModified.equals("") && ifModifiedDate.indexOf("GMT") != -1) {
+                try {
+                    dateAfter = dateIsAfter(lastModified, ifModifiedDate);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                if (!dateAfter) {
+                    output.print("HTTP/1.0 304 Not Modified\r\n");
+                    output.print("\r\n");
+                    killThread();
+                    return;
+                }
+            }
+
+            //Sends 200 OK response headers, sends file data, closes output stream, returns
+            output.print("HTTP/1.0 200 OK\r\n");
+            output.print("Content-Type: " + mimeType + "\r\n"); // TODO - add mime type support
+            output.print("Content-Length: " + file.length() + "\r\n");
+            output.print("Last-Modified: " + lastModified + "\r\n");
 
             //Headers for all 200 OK responses
             output.print("Allow: GET, POST, HEAD\r\n");
             output.print("Content-Encoding: identity\r\n");
-            output.print("Expires: Wed, 03 Mar 2021 01:35:39 GMT\r\n");
+            output.print("Expires: Wed, 02 Oct 2024 01:37:39 GMT\r\n");
 
             output.print("\r\n"); // End of headers
             output.write(data);
@@ -196,9 +227,20 @@ public class PartialHTTP1Threads extends Thread{
         return format.format(calendar.getTime());
     }
 
+    private boolean dateIsAfter(String lastModifiedDate, String ifModifiedAfter) throws ParseException {
+        lastModifiedDate = lastModifiedDate.replace("GMT", "").trim();
+        ifModifiedAfter = ifModifiedAfter.replace("GMT", "").trim();
+        lastModifiedDate = lastModifiedDate.substring(5, lastModifiedDate.length());
+        ifModifiedAfter = ifModifiedAfter.substring(5, ifModifiedAfter.length());
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy MMM dd HH:mm:ss");
+        long lastMod = format.parse(lastModifiedDate).getTime();
+        long ifModAfter = format.parse(ifModifiedAfter).getTime();
+        return (lastMod > ifModAfter);
+    }
+
     private String formatMimeType(String mimeType) {
         String returnMime = mimeType;
-        System.out.println("\n\n\n" + mimeType + "\n\n\n");
         if (!(mimeType.equals("text/html")) && !(mimeType.equals("text/plain")) && !(mimeType.equals("image/gif")) &&
                         !(mimeType.equals("image/jpeg")) && !(mimeType.equals("image/png")) && !(mimeType.equals("application/pdf")) &&
                         !(mimeType.equals("application/x-gzip")) && !(mimeType.equals("application/zip"))) {
