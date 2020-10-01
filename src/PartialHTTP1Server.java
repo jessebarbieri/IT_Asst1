8c4c41f3d010c9def2662d41be7295c0b51f78f5
Jesse Barbieri <jsb306@scarletmail.rutgers.edu>
2020-10-01 15:14:47 -0400

    add argument to linkedblockqueue

--- src/PartialHTTP1Server.java
+++ src/PartialHTTP1Server.java
@@ -37,7 +37,7 @@ public class PartialHTTP1Server {
         }
 
         //Creates threadPool with maximum of 50 threads
-        ThreadPoolExecutor threadPool =  new ThreadPoolExecutor(5, 50, 250, TimeUnit.MILLISECONDS, new LinkedBlockingDeque<>());
+        ThreadPoolExecutor threadPool =  new ThreadPoolExecutor(5, 50, 250, TimeUnit.MILLISECONDS, new LinkedBlockingDeque<>(1));
 
         //Server loop, waits for socket connection
         while(true) {
